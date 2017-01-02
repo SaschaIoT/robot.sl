98a5f249d3ee607298bae998b07e2e6fb177920a
Sascha <dummy@dummy.de>
2017-01-02 23:41:39 +0100

    Bug fix: Wait unitl async Dispatcher method has completed

---
+++ robot.sl/Helper/DispatcherHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace robot.sl.Helper
+{
+    public static class DispatcherHelper
+    {
+        public static async Task RunAndAwaitAsync(this CoreDispatcher dispatcher, CoreDispatcherPriority priority, Func<Task> asyncAction)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            await dispatcher.RunAsync(priority, async () =>
+            {
+                try
+                {
+                    await asyncAction().ConfigureAwait(false);
+
+                    taskCompletionSource.TrySetResult(true);
+                }
+                catch (Exception ex)
+                {
+                    taskCompletionSource.TrySetException(ex);
+                }
+            });
+
+            await taskCompletionSource.Task.ConfigureAwait(false);
+        }
+    }
+}
