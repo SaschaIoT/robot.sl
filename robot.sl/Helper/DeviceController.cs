26e47506b15983624a89b493e2fe4a36d77b7d23
SaschaIoT <mail@saschalehmann.de>
2017-12-24 15:26:46 +0100

    Add comment

--- robot.sl/Helper/DeviceController.cs
+++ robot.sl/Helper/DeviceController.cs
@@ -8,6 +8,9 @@ namespace robot.sl.Helper
 {
     public static class DeviceController
     {
+        //To allow to run IoTCoreAudioControlTool.exe and Powershell.exe from UWP apps, execute the following command with powershell on the Windows IoT Core device
+        //reg.exe ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\EmbeddedMode\ProcessLauncher" /f /v AllowedExecutableFilesList /t REG_MULTI_SZ /d "C:\Windows\System32\IoTCoreAudioControlTool.exe\0C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe\0"
+
         public static async Task ShutdownDevice()
         {
             await ExecuteCommand("shutdown -s -t 0");
