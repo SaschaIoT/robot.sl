e044ac5aba9bf995bfa0194ff00ae4287029b246
SaschaIoT <mail@saschalehmann.de>
2018-01-30 21:43:06 +0100

    Replace all servos

--- robot.sl/Helper/ServoHelper.cs
+++ robot.sl/Helper/ServoHelper.cs
@@ -10,19 +10,19 @@
 
     public static class ServoPositions
     {
-        public static ushort CameraHorizontalMiddle { get; } = 279;
+        public static ushort CameraHorizontalMiddle { get; } = 283;
 
-        public static ushort CameraVerticalMiddle { get; } = 220;
-        public static ushort CameraVerticalTop { get; } = 160;
-        public static ushort CameraVerticalBottom { get; } = 320;
+        public static ushort CameraVerticalMiddle { get; } = 280;
+        public static ushort CameraVerticalTop { get; } = 210;
+        public static ushort CameraVerticalBottom { get; } = 370;
 
-        public static ushort DistanceSensorHorizontalLeft { get; } = 95;
-        public static ushort DistanceSensorHorizontalMiddle { get; } = 279;
-        public static ushort DistanceSensorHorizontalLeftMiddle { get; } = 170;
-        public static ushort DistanceSensorHorizontalRight { get; } = 455;
-        public static ushort DistanceSensorHorizontalRightMiddle { get; } = 370;
+        public static ushort DistanceSensorHorizontalLeft { get; } = 120;
+        public static ushort DistanceSensorHorizontalLeftMiddle { get; } = 180;
+        public static ushort DistanceSensorHorizontalMiddle { get; } = 295;
+        public static ushort DistanceSensorHorizontalRight { get; } = 470;
+        public static ushort DistanceSensorHorizontalRightMiddle { get; } = 390;
 
-        public static ushort DistanceSensorVerticalTop { get; } = 130;
-        public static ushort DistanceSensorVerticalMiddle { get; } = 245;
+        public static ushort DistanceSensorVerticalTop { get; } = 160;
+        public static ushort DistanceSensorVerticalMiddle { get; } = 252;
     }
 }
